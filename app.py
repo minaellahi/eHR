a8fff98670d7c7c76426f89982457179859d654d
Atif Khan <78403730+atifviews@users.noreply.github.com>
2023-04-23 19:44:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask
+
+
+#mongo imports
+from flask import Flask
+from pymongo import MongoClient
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
+#TASK 01
 #pull code from github"
 #create "templates" folder in main directory with app.py
-
 #add "home.html" file in "templates". 
 #then visit "http://127.0.0.1:8000/"  you will see you home page
 #same way create "sigin","signup", "candidate_dashboard", "recruitment_dashboard","candidate_profile" etc
 #push code from github
 
+
+
+#TASK02
+#connect flask app with mongoDB using following code. replace hostname and port with the actual one.
+"""client = MongoClient('mongodb://<hostname>:<port>/')
+db = client['your_database_name']
+"""
 
 
 if __name__ == '__main__':
