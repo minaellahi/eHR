cf2717b15af22d95f63d689b9928934556c297b5
Atif Khan <78403730+atifviews@users.noreply.github.com>
2023-04-23 19:36:52 +0500

    simple pages guideline in comments.

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+#pull code from github"
+#create "templates" folder in main directory with app.py
+
+#add "home.html" file in "templates". 
+#then visit "http://127.0.0.1:8000/"  you will see you home page
+#same way create "sigin","signup", "candidate_dashboard", "recruitment_dashboard","candidate_profile" etc
+#push code from github
+
+
 
 if __name__ == '__main__':
     app.run(debug == 'True')
